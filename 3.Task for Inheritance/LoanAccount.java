b14ae7110b4461f6161e515df154e4a94f7d0982
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-03-17 17:50:31 +0530

    Code Generated

---
+++ 3.Task for Inheritance/LoanAccount.java
@@ -0,0 +1,8 @@
+
+public class LoanAccount extends Account {
+
+    public LoanAccount(int accountNO, String name, String address, int phoneNo, int dob, int balance) {
+        super(accountNO, name, address, phoneNo, dob, balance);
+
+    }
+}
